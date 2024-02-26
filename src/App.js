682ca610224a98d4bafc2170c01be8d8a56d817d
YashGitt10 <yashrohitgupt@gmail.com>
2024-02-26 13:43:03 +0530

    cnter via react

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  const[count, setCount] = useState(0);
+  function decrement() {
+    setCount(count-1);
+  }
+
+  function increment() {
+    setCount(count+1);
+  }
+
+  function reset() {
+    setCount(0);
+  }
+  return (
+    <div className="w-[100vw] h-[100vh] flex justify-center items-center bg-[#344151] flex-col gap-10">
+
+      <div className="text-[#0398d4] font-medium text-2xl">
+        Increment and Decrement
+      </div>
+
+      <div className="bg-white flex justify-center gap-12 py-3 rounded-sm text-[25px] text-[#344151]">
+
+        <button onClick={decrement} className="border-r-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          -
+        </button>
+
+        <div className="font-bold text-5xl gap-12">
+          {count}
+        </div>
+
+        <button onClick={increment} className="border-l-2 text-center w-20 border-[#bfbfbf] text-5xl">
+          +
+        </button>
+
+      </div>
+      <button onClick={reset} className="px-5 py-2 rounded-sm text-lg bg-[#0398d4]">
+        RESET
+      </button>
+    </div>
+  );
+}
+
+export default App;
